d1fcbefc3c3259c6878839c0628008fa2c184ca7
accesendomain <accesendomain>
2023-10-12 07:41:49 +0000

    Pushed from Locofy Builder

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,59 @@
-import TopNewsCard from "../components/TopNewsCard";
-import HeaderPublic from "../components/HeaderPublic";
 import styles from "./Homepage.module.css";
 
 const Homepage = () => {
   return (
     <div className={styles.homepage}>
-      <TopNewsCard />
-      <HeaderPublic />
+      <div className={styles.contentstand}>
+        <div className={styles.container}>
+          <div className={styles.topstorie}>
+            <div className={styles.headlinecontainer}>
+              <div className={styles.storydescription}>
+                <span className={styles.storydescriptionTxt}>
+                  {`Airstrikes `}
+                  <a
+                    className={styles.smashedEntireCityBlocks}
+                    href="https://apnews.com/6306a3a4ef854bdcb2e3e698e6bd4f02"
+                    target="_blank"
+                  >
+                    <span className={styles.smashedEntireCity}>
+                      smashed entire city blocks
+                    </span>
+                  </a>{" "}
+                  to rubble in the tiny coastal enclave and left unknown numbers
+                  of bodies beneath mounds of debris.
+                </span>
+              </div>
+              <b className={styles.headlinetitle}>
+                <span className={styles.storydescriptionTxt}>
+                  <p className={styles.trumpDipsInto}>
+                    Trump dips into internet conspiracy theories with his latest
+                    lines about Obama
+                  </p>
+                </span>
+              </b>
+            </div>
+          </div>
+        </div>
+      </div>
+      <header className={styles.headerPublic} id="header-public">
+        <div className={styles.uppermenu}>
+          <a className={styles.zobuoUipng} href="https://www.zobuo.com" />
+          <div className={styles.btncontainer}>
+            <button className={styles.appdrawerbtn} id="AppDrawerbtn" />
+            <div className={styles.signinbtn}>
+              <div className={styles.signIn}>Sign In</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.lowermenu}>
+          <button className={styles.homepagebtn} id="homepagebtn">
+            <div className={styles.homepage1}>Homepage</div>
+          </button>
+          <button className={styles.everythingbtn} id="homepagebtn">
+            <div className={styles.everything}>Everything</div>
+          </button>
+        </div>
+      </header>
     </div>
   );
 };
--- src/pages/PublicProfile.js
+++ src/pages/PublicProfile.js
@@ -1,12 +1,70 @@
-import ProfileCard from "../components/ProfileCard";
-import HeaderPublic from "../components/HeaderPublic";
 import styles from "./PublicProfile.module.css";
 
 const PublicProfile = () => {
   return (
     <div className={styles.publicprofile}>
-      <ProfileCard />
-      <HeaderPublic />
+      <div className={styles.profilecontainer}>
+        <div className={styles.profilecard}>
+          <img
+            className={styles.profilecoverIcon}
+            alt=""
+            src="/profilecover@2x.png"
+          />
+          <img
+            className={styles.profiledpIcon}
+            alt=""
+            src="/profiledp@2x.png"
+          />
+          <div className={styles.profileinfocont}>
+            <div className={styles.profilename}>
+              <b className={styles.username}>Yeshan Peiris</b>
+              <div className={styles.verifiedstatus} />
+            </div>
+            <div className={styles.handlecontainer}>
+              <div className={styles.username}>@yeshanpeiris</div>
+            </div>
+          </div>
+          <div className={styles.profileminicard}>
+            <div className={styles.biodatacontainer}>
+              <div className={styles.joinedate}>
+                <img
+                  className={styles.joinedIcon}
+                  alt=""
+                  src="/joined-icon@2x.png"
+                />
+                <div className={styles.username}>Joined Dec 5, 2002</div>
+              </div>
+              <div className={styles.locationcontainer}>
+                <img
+                  className={styles.joinedIcon}
+                  alt=""
+                  src="/location-icon@2x.png"
+                />
+                <div className={styles.username}>Panadura, LK</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <header className={styles.headerPublic} id="header-public">
+        <div className={styles.uppermenu}>
+          <a className={styles.zobuoUipng} href="https://www.zobuo.com" />
+          <div className={styles.btncontainer}>
+            <button className={styles.appdrawerbtn} id="AppDrawerbtn" />
+            <div className={styles.signinbtn}>
+              <div className={styles.signIn}>Sign In</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.lowermenu}>
+          <button className={styles.homepagebtn} id="homepagebtn">
+            <div className={styles.homepage}>Homepage</div>
+          </button>
+          <button className={styles.everythingbtn} id="homepagebtn">
+            <div className={styles.everything}>Everything</div>
+          </button>
+        </div>
+      </header>
     </div>
   );
 };
